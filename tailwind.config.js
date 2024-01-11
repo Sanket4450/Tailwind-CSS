acb4d6bbfa2d6b5a37af3dfd2f7f0ceaf9a909c1
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-01-11 19:41:48 +0530

    half page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'tab': '834px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
+    }
   },
   plugins: [],
 }
